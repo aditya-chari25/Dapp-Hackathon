b3c4d20ba3e9fb9355cd05025af14809c37ffdc1
unknown <adi25charis@gmail.com>
2023-12-31 15:21:59 +0530

    changes in backend infrastructure

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const io = require("socket.io")(http);
 const inquirer = require("inquirer");
 const open = require("open");
 const TextDecoder = require("text-encoding").TextDecoder;
+const PORT = 8080
 
 /* hedera.js */
 const {
@@ -27,11 +28,146 @@ const UInt8ToString = require("./utils.js").UInt8ToString;
 const secondsToDate = require("./utils.js").secondsToDate;
 const log = require("./utils.js").handleLog;
 const sleep = require("./utils.js").sleep;
+app.use(express.urlencoded());
 
 let operatorAccount = "";
 const hederaClient = Client.forTestnet();
 let topicId = "";
 let logStatus = "Default";
+
+app.get('/form', function(request, response, next){
+
+	response.send(`
+    <!DOCTYPE html>
+    <style>
+    body{
+        background : rgb(49,51,56);
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        align-items:center;
+        text-align:center
+    }
+
+    .create{
+        color : rgb(221,222,224);
+    }
+    
+    .create {
+        max-width: 400px;
+        margin: 0 auto;
+        text-align: center;
+      }
+      .create label {
+        text-align: left;
+        display: block;
+        color:rgb(221,222,224);
+      }
+      .create h2 {
+        font-size: 20px;
+        color: rgb(221,222,224);
+        margin-bottom: 30px;
+      }
+      .create input, .create textarea, .create select {
+        width: 100%;
+        padding: 6px 10px;
+        margin: 10px 0;
+        border: 1px solid black;
+        border-radius:10px;
+        box-sizing: border-box;
+        display: block;
+      }
+      .create button {
+        background: white;
+        color: black;
+        border: 0;
+        padding: 8px;
+        border-radius: 18px;
+        cursor: pointer;
+      }
+    
+      ul{
+        font-size: 12px;
+        text-align: center;
+      }
+    
+      .boxer{
+        border: 1px solid grey;
+        align-items: center;
+        text-align: center;
+        margin-top: 2vh;
+      }
+      </style>
+      <body>
+
+    <div class="create" style="max-width:400px; margin:0 auto">
+      <h1>Dapp Chat App</h1>
+      <p>Connect to exiting room or connect to new room</p>
+      <form method="POST" action="/">
+        <label>Account:</label>
+        <input type="text"id = "account"name = "account" />
+        <label>Key:</label>
+        <input type="text" id = "key"name = "key"/>
+        <label>Topic:</label>
+        <input type="text" id = "topic"name = "topic"value="create a new topic" style="border-radius:10px" />
+        <button>Create Room</button>
+      </form>
+      <div class="boxer">
+      <ul><b>Rules for using:-</b></ul>
+      <ul>1. Empty account, key will connect to default config present</ul>
+      <ul>2. Create a new topic will create new discussion room</ul>
+      <ul>3. if want to connect to existing room, enter that room config</ul>
+      <ul>4. Wait for 20sec after creating a room, you'll be directed to a new page</ul>
+      </div>
+    </div>
+    </body>
+    </html>
+`);
+
+
+});
+
+app.post('/', async(req, res) => {
+    try {
+        let temp = req.body
+        let answers = {
+            status : 'default',
+            account : temp.account,
+            key : temp.key,
+            topic: temp.topic
+        }
+        console.log(answers)
+        logStatus = answers.status;
+        console.log(answers.status)
+        configureAccount(answers.account, answers.key);
+        if (answers.topic != undefined) {
+            configureExistingTopic(answers.topic);
+        } else {
+            await configureNewTopic();
+        }
+        /* run & serve the express app */
+        runChat();
+    } 
+    catch (error) {
+        log("ERROR: init() failed", error, logStatus);
+        process.exit(1);
+    }
+})
+
+function generateRandomPort() {
+    const minPort = 3000;
+    const maxPort = 9000;
+  
+    return Math.floor(Math.random() * (maxPort - minPort + 1)) + minPort;
+}
+const port123 = generateRandomPort();
+
+app.listen(port123, () => {
+    console.log(`Example app listening on port ${port123}`)
+})
 
 /* configure our env based on prompted input */
 async function init() {
@@ -91,7 +227,7 @@ function runChat() {
     });
 }
 
-init(); // process arguments & handoff to runChat()
+// init(); // process arguments & handoff to runChat()
 
 /* helper hedera functions */
 
